782ed903daef596d7d174d8a9f96000e301a7fb5
Abigail A <aagui2797@gmail.com>
2021-10-19 02:35:37 -0700

    create function for password requirements

--- script.js
+++ script.js
@@ -18,6 +18,30 @@ function arraylowtohigh (low, high) {
   }
   return array
 }
+function passwordRequirements() {
+  var length = parseInt(
+    prompt('How many characters shall your password contain?')
+  );
+
+  if (length < 8) {
+    alert('Password lenghth requires a minimum of 8 characters!');
+    return null;
+  }
+
+  if (length >128) {
+    alert('Password length must be at maximum of 128 chracters!');
+    return null;
+  }
+
+  var uppercaseCharacters = confirm('Do you confirm to add uppercase character? Click OK to accept.');
+
+  var lowercaseCharacters = confirm('Do you confirm to add lowercase characters? Click OK to accept');
+
+  var numberCharacter = confirm('Do you confirm to add numbers? Click OK to accept');
+
+  var specialCharacters = confirm('Do you confirm to add special characters? Click OK to accept');
+
+}
 
 // Write password to the #password input
 function writePassword() {
